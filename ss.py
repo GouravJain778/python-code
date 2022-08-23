97ce8d30c7685b8e0501dbf16281cf8d1171df37
gouravJain778 <gouravjain778@gmail.com>
2022-08-24 01:55:49 +0530

    code uncomment

--- ss.py
+++ ss.py
@@ -1,8 +1,8 @@
-# l=[1,2,3,4,5]
-# l1=[2,3,4,5,6,7,8,9]
-# for i in range(0,len(l)):
-#     if i in l1:
-#         print(i)
+l=[1,2,3,4,5]
+l1=[2,3,4,5,6,7,8,9]
+for i in range(0,len(l)):
+    if i in l1:
+        print(i)
 s='there is a 1 bottel'
 ss=list(s)
 for i in range(0,len(ss)):
